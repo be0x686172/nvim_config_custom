20fb5bfe3602f2f195f3201150840788f51c055a
be0x686172 <abderahmane.beharrahala@gmail.com>
2025-11-16 04:34:15 +0100

    nvim_config_custom

---
+++ lua/core/options.lua
@@ -0,0 +1,10 @@
+vim.opt.tabstop = 4;
+vim.opt.softtabstop = 4;
+vim.opt.shiftwidth = 4;
+
+vim.opt.number = true;
+vim.opt.cursorline = true;
+vim.opt.list = true;
+vim.opt.listchars = {tab = "→ ", trail = "."};
+
+vim.opt.clipboard = "unnamed";
