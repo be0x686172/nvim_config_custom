10d155facf8243b361a1cda2d1891b384793876d
be0x686172 <abderahmane.beharrahala@gmail.com>
2025-11-16 19:27:10 +0100

    nvim_config_custom

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,2 +1,10 @@
 -- Telescope
 vim.keymap.set("n", "<leader>f", "<cmd>Telescope find_files<CR>")
+
+-- Sauvegarder le fichier
+vim.keymap.set("n", "<C-s>", "<cmd>w<CR>", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-s>", "<Esc><cmd>w<CR>a", { noremap = true, silent = true })
+
+-- Fermer sans sauvegarder
+vim.keymap.set("n", "<C-q>", "<cmd>qa!<CR>", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-q>", "<Esc><cmd>qa!<CR>", { noremap = true, silent = true })
